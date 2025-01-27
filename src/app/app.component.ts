68831643d996c4197e83cd06cd40dde1657c2e79
okavango <eguilhermeleitedev81@gmail.com>
2025-01-27 13:08:20 -0300

    component navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgClass, NgIf} from '@angular/common';
+import {NavbarComponent} from '../component/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    NgIf,
+    NgClass,
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'edplus-frontend';
+
+
+
 }
---
+++ src/component/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import {Component, HostListener} from '@angular/core';
+import {NgClass} from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    NgClass
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+  isScrolled = false;
+
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+
+    // Verifica se a rolagem ultrapassa 20px
+    this.isScrolled = window.scrollY > 20;
+    console.log(window.scrollY);
+  }
+}
